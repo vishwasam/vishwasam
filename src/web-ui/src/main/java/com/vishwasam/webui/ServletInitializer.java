d3b9662faac31a72d112ff03b5c4d2e755bfe592
sanjay <sanjay.chaudhary@accoliteindia.com>
2020-07-15 17:41:03 +0530

    Updating initial project

---
+++ src/web-ui/src/main/java/com/vishwasam/webui/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.vishwasam.webui;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebUiApplication.class);
+	}
+
+}
